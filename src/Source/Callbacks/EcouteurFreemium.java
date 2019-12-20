d9f96ce7d0022211e0f9749b7f05938e53a663e7
Serge SULA <sulabosiog@gmail.com>
2019-12-20 23:54:04 +0100

    Merge origin/master
    Conflicts:
    	build/built-jar.properties
    	build/classes/SOURCES/Objets/FileManager.class
    	dist/JS2BFileManager.jar
    	nbproject/private/private.properties
    	nbproject/private/private.xml

---
+++ src/Source/Callbacks/EcouteurFreemium.java
@@ -0,0 +1,25 @@
+package Source.Callbacks;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author HP Pavilion
+ */
+public abstract class EcouteurFreemium {
+    public abstract boolean onVerifie();
+}
+
+
+
+
+
+
+
